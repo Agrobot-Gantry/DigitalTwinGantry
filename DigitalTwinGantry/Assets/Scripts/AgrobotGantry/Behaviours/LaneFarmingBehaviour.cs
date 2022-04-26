fe9cc1e555d81bc9d42750db7ca26626bd3396bc
noahwalsmits <noahwalsmits@telfort.nl>
2022-04-26 15:52:36 +0200

    Updated example behaviour

--- DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Behaviours/LaneFarmingBehaviour.cs
+++ DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Behaviours/LaneFarmingBehaviour.cs
@@ -15,23 +15,29 @@ public class LaneFarmingBehaviour : AgrobotBehaviour
     public override void Start(AgrobotGantry agrobotGantry)
     {
         base.Start(agrobotGantry);
-        m_gantry.MovementSpeed = 1.0f;
         m_gantry.TurningSpeed = 0.0f;
     }
 
     public override void Update(float deltaTime)
     {
-        if (this.m_gantry.Equipment.GetReachables().Length > 0)
+        //TODO add callback from equipment to notify if new valid interactables are in reach
+        if (m_ongoingActions.Count == 0)
         {
-            //stop and start action
-            //continue driving when all have finished
-            m_gantry.MovementSpeed = 0.0f;
-            StartAction(new HarvestAction(this, this.m_gantry.Equipment.GetReachables()[0], this.m_gantry.Equipment));
+            m_gantry.MovementSpeed = 1.0f; //keep driving
+            if (this.m_gantry.Equipment.GetReachables().Length > 0) //check reach
+            {
+                StartAction(new HarvestAction(this, this.m_gantry.Equipment.GetReachables()[0], this.m_gantry.Equipment));
+            }
         }
+        else
+        {
+            m_gantry.MovementSpeed = 0.0f; //stop until the action is complete
+        }
+        
     }
 
     public override void Stop()
     {
-        throw new System.NotImplementedException();
+        
     }
 }
