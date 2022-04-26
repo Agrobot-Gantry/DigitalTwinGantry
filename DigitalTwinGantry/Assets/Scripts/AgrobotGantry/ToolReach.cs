e92606c7a7d4f883a1fe944c250d7be72ba97a55
noahwalsmits <noahwalsmits@telfort.nl>
2022-04-26 17:30:00 +0200

    Visualised tool reach

--- DigitalTwinGantry/Assets/Scripts/AgrobotGantry/ToolReach.cs
+++ DigitalTwinGantry/Assets/Scripts/AgrobotGantry/ToolReach.cs
@@ -7,12 +7,18 @@ using UnityEngine;
 /// </summary>
 public class ToolReach : MonoBehaviour
 {
+    [Tooltip("show the reach area visual inside of the game instead of just the editor")]
+    public bool showReach = false;
+    //TODO make the reach only count interactables if their center is inside?
+    //TODO or change interactables to have very small hitboxes
+
     private delegate void ToolCallback(AgrobotInteractable interactable);
     private List<ToolCallback> m_enterCallbacks;
     private List<ToolCallback> m_exitCallbacks;
 
     void Awake()
     {
+        GetComponent<MeshRenderer>().enabled = showReach; //the reach area should only be visible in the editor
         m_enterCallbacks = new List<ToolCallback>();
         m_exitCallbacks = new List<ToolCallback>();
     }
