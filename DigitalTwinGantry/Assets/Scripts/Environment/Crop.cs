0ba721b09eae54ff06eae0ccc6b13f1f36677ed2
noahwalsmits <noahwalsmits@telfort.nl>
2022-05-11 11:43:27 +0200

    [ADDED] implemented method to get sowing timeperiod

--- DigitalTwinGantry/Assets/Scripts/Environment/Crop.cs
+++ DigitalTwinGantry/Assets/Scripts/Environment/Crop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,9 @@ public class Crop : MonoBehaviour
 
 	private delegate void OnHarvestCallback(Crop crop);
 	private OnHarvestCallback m_callback;
-	private int m_timePeriodOffset;
+	private TimePeriod m_currentTimePeriod;
+	private int m_timePeriodOffset; //the difference between the desired timeperiod and the real one
+									//chunks might decide to grow a crop a little earlier or later after the previous was harvested
 
 	[System.Serializable]
 	private struct TimePeriod
@@ -23,12 +26,20 @@ public class Crop : MonoBehaviour
 	{
 		m_callback = new OnHarvestCallback(cropField.OnCropHarvested);
 		m_timePeriodOffset = timePeriodOffset;
+
+		foreach (TimePeriod timePeriod in m_timePeriods)
+		{
+			timePeriod.Model.SetActive(false);
+		}
 	}
 
-	public void UpdateTimePeriod(int timePeriod)
+	public void UpdateTimePeriod(int newTimePeriod)
 	{
-		//TODO
-		//keep offset in mind then updating
+		m_currentTimePeriod.Model.SetActive(false);
+		m_currentTimePeriod = m_timePeriods[(newTimePeriod + m_timePeriodOffset) % TIME_PERIOD_COUNT];
+		m_currentTimePeriod.Model.SetActive(true);
+
+		//TODO apply new flags to interactable
 	}
 
 	public void OnInteract(AgrobotAction action)
@@ -44,17 +55,53 @@ public class Crop : MonoBehaviour
 		}
 	}
 
-	public int GetSowingTimePeriod()
+	/// <summary>
+	/// Returns the index of a timeperiod that has the SOW interactable flag. 
+	/// If there are multiple timeperiods with the SOW flag this will return the one closest to the parameter.
+	/// If there are no timeperiods with the SOW flag this will return int.MaxValue.
+	/// </summary>
+	/// <param name="comparedTimePeriod">the timeperiod the returned value should be closest to</param>
+	/// <returns>the index of a timeperiod that has the SOW interactable flag</returns>
+	public int GetNearestSowingTimePeriod(int comparedTimePeriod)
 	{
-		//TODO
-		return 0;
+		//find all time periods with the sowing flag
+		List<int> sowingTimePeriods = new List<int>();
+		for(int i = 0; i < TIME_PERIOD_COUNT; i++)
+		{
+			if (m_timePeriods[i].InteractableFlags.HasFlag(InteractableFlag.SOW))
+			{
+				sowingTimePeriods.Add(i);
+			}
+		}
+
+		//find the one nearest to the parameter timeperiod
+		int nearestIndex = int.MaxValue;
+		foreach (int index in sowingTimePeriods)
+		{
+			if (DistanceBetween(comparedTimePeriod, index) < DistanceBetween(comparedTimePeriod, nearestIndex))
+			{
+				nearestIndex = index;
+			}
+		}
+		return nearestIndex;
+	}
+
+	/// <returns>the distance between two timeperiods</returns>
+	private int DistanceBetween(int timePeriod1, int timePeriod2)
+	{
+		int difference = Math.Abs(timePeriod1 - timePeriod2);
+		if (difference > TIME_PERIOD_COUNT / 2)
+		{
+			difference = TIME_PERIOD_COUNT - difference;
+		}
+		return difference;
 	}
 
 	void OnValidate()
 	{
 		if (m_timePeriods.Length != TIME_PERIOD_COUNT)
 		{
-			Debug.LogWarning("Each crop must have exactly " + TIME_PERIOD_COUNT + " time periods!");
+			Debug.LogError("Each crop must have exactly " + TIME_PERIOD_COUNT + " time periods!");
 			System.Array.Resize(ref m_timePeriods, TIME_PERIOD_COUNT);
 		}
 	}
