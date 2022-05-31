a7c7ef49a77d896a4bf5ca84f01b62807eadb964
Menno <my.bil@student.avans.nl>
2022-05-31 12:15:59 +0200

    [ADDED] inverse kinematics arms

--- DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Equipment/InKinematicArm.cs
+++ DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Equipment/InKinematicArm.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class InKinematicArm : MonoBehaviour
 {
-    [SerializeField] private Vector3 point = new Vector3(0, -5, 0);
-
     [SerializeField] private GameObject m_segmentPrefab;
+    [SerializeField] private Vector3 point = new Vector3(0, -5, 0);
     [SerializeField] private int m_totalSegments;
+    [SerializeField] private bool m_isAttached;
 
     private List<InKinematicSegment> m_segments;
+    private Vector3 m_basePoint;
 
     private void Start()
     {
@@ -20,6 +21,11 @@ public class InKinematicArm : MonoBehaviour
             GameObject segment = Instantiate(m_segmentPrefab, transform);
             segment.transform.localPosition = new Vector3(segment.transform.localPosition.x, segment.transform.localPosition.y + addY, segment.transform.localPosition.z);
             m_segments.Add(segment.GetComponent<InKinematicSegment>());
+
+            if (i == 0)
+            {
+                m_basePoint = segment.transform.position;
+            }
 
             addY += segment.transform.localScale.y;
         }
@@ -34,12 +40,21 @@ public class InKinematicArm : MonoBehaviour
     {
         InKinematicSegment end = m_segments[m_segments.Count - 1];
         end.Follow(point);
-        end.CalculateEndPos();
 
         for (int i = m_segments.Count - 2; i >= 0; i--)
         {
             m_segments[i].Follow(m_segments[i + 1].transform.position);
-            m_segments[i].CalculateEndPos();
+        }
+
+        if (!m_isAttached)
+        {
+            return;
+        }
+
+        m_segments[0].gameObject.transform.position = m_basePoint;
+        for (int i = 1; i < m_segments.Count; i++)
+        {
+            m_segments[i].gameObject.transform.position = m_segments[i - 1].EndPos;
         }
     }
 }
--- DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Equipment/InKinematicSegment.cs
+++ DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Equipment/InKinematicSegment.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class InKinematicSegment : MonoBehaviour
 {
+    [SerializeField] private Transform m_endPos;
+
+    public Vector3 EndPos { get => m_endPos.position; }
+
     public void Follow(Vector3 target)
     {
         Vector3 direction = target - transform.position;
@@ -16,7 +20,6 @@ public class InKinematicSegment : MonoBehaviour
         }
 
         float yRotation = Mathf.Atan((target.z - transform.position.z) / value);
-        // float yRotation = Mathf.Atan((target.z - transform.position.z) / (target.x - transform.position.x));
 
         float sinTheta = Mathf.Sin(-yRotation);
         float cosTheta = Mathf.Cos(-yRotation);
@@ -38,10 +41,14 @@ public class InKinematicSegment : MonoBehaviour
         transform.position = target + direction;
     }
 
-    public void CalculateEndPos()
-    {
-        // TODO
-    }
+    // public void CalculateEndPos()
+    // {
+    //     // float dx = transform.lossyScale.y * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.z) * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.y);
+    //     float dy = transform.lossyScale.y * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z);
+    //     // float dz = transform.lossyScale.y * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.z) * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.y);
+    //
+    //     EndPos = new Vector3(transform.position.x, transform.position.y - dy, transform.position.z);
+    // }
 
     public static Vector3 GetAngle(Vector3 from, Vector3 to)
     {
