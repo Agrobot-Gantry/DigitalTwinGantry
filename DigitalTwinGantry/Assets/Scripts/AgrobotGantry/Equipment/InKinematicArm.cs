f0873f98647cda51c3a25fdac802b5f18952ce9d
Menno <my.bil@student.avans.nl>
2022-05-31 14:02:28 +0200

    [ADDED] arm prefab

--- DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Equipment/InKinematicArm.cs
+++ DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Equipment/InKinematicArm.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class InKinematicArm : MonoBehaviour
 {
     [SerializeField] private GameObject m_segmentPrefab;
-    [SerializeField] private Vector3 point = new Vector3(0, -5, 0);
+    [SerializeField] private Vector3 reachPoint;
+    [SerializeField] private Transform m_basePoint;
     [SerializeField] private int m_totalSegments;
     [SerializeField] private bool m_isAttached;
 
     private List<InKinematicSegment> m_segments;
-    private Vector3 m_basePoint;
 
     private void Start()
     {
@@ -22,18 +22,13 @@ public class InKinematicArm : MonoBehaviour
             segment.transform.localPosition = new Vector3(segment.transform.localPosition.x, segment.transform.localPosition.y + addY, segment.transform.localPosition.z);
             m_segments.Add(segment.GetComponent<InKinematicSegment>());
 
-            if (i == 0)
-            {
-                m_basePoint = segment.transform.position;
-            }
-
             addY += segment.transform.localScale.y;
         }
     }
 
     private void Update()
     {
-        ReachForPoint(point);
+        ReachForPoint(reachPoint);
     }
 
     private void ReachForPoint(Vector3 point)
@@ -51,7 +46,7 @@ public class InKinematicArm : MonoBehaviour
             return;
         }
 
-        m_segments[0].gameObject.transform.position = m_basePoint;
+        m_segments[0].gameObject.transform.position = m_basePoint.position;
         for (int i = 1; i < m_segments.Count; i++)
         {
             m_segments[i].gameObject.transform.position = m_segments[i - 1].EndPos;
