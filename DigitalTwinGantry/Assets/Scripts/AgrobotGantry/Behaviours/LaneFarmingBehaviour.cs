60a5cf21e235fd99fd801288a9a02f4a5b2a02cf
noahwalsmits <noahwalsmits@telfort.nl>
2022-06-22 10:17:33 +0200

    [CHANGED] formatted lanefarmingbehaviour

--- DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Behaviours/LaneFarmingBehaviour.cs
+++ DigitalTwinGantry/Assets/Scripts/AgrobotGantry/Behaviours/LaneFarmingBehaviour.cs
@@ -7,42 +7,40 @@ using UnityEngine;
 /// </summary>
 public class LaneFarmingBehaviour : AgrobotBehaviour
 {
-    public LaneFarmingBehaviour() : base()
-    {
+	public LaneFarmingBehaviour() : base()
+	{
 
-    }
+	}
 
-    public override void Start(AgrobotGantry agrobotGantry)
-    {
-        base.Start(agrobotGantry);
-        m_gantry.TurningSpeed = 0.0f;
-    }
+	public override void Start(AgrobotGantry agrobotGantry)
+	{
+		base.Start(agrobotGantry);
+		m_gantry.TurningSpeed = 0.0f;
+	}
 
-    public override void Update(float deltaTime)
-    {
-        foreach (AgrobotTool tool in m_gantry.tools)
-        {
-            if (tool.goingTooFast)
-            {
-                m_gantry.MovementSpeed = 0.0f;
-                break;
-            }
-            m_gantry.MovementSpeed = AgrobotDefinitions.Instance.MovementSpeed; //keep driving
-        }
-           
-            if (this.m_gantry.Equipment.GetReachables().Length > 0) //check reach
-            {
-                foreach(AgrobotInteractable reachable in m_gantry.Equipment.GetReachables())
-                {
-                    AgrobotAction action = CreateAction(reachable);
-                    if(action != null)
-                    {
-                        StartAction(action);
-                    }
-                }
-                //AgrobotAction action = m_gantry.Equipment.GetReachables()[0].GetActions(this, m_gantry.Equipment); //for now we just assume there's just one in reach
-            }
-        }
-    }
+	public override void Update(float deltaTime)
+	{
+		foreach (AgrobotTool tool in m_gantry.tools)
+		{
+			if (tool.goingTooFast)
+			{
+				m_gantry.MovementSpeed = 0.0f;
+				break;
+			}
+			m_gantry.MovementSpeed = AgrobotDefinitions.Instance.MovementSpeed; //keep driving
+		}
 
+		if (this.m_gantry.Equipment.GetReachables().Length > 0) //check reach
+		{
+			foreach (AgrobotInteractable reachable in m_gantry.Equipment.GetReachables())
+			{
+				AgrobotAction action = CreateAction(reachable);
+				if (action != null)
+				{
+					StartAction(action);
+				}
+			}
+			//AgrobotAction action = m_gantry.Equipment.GetReachables()[0].GetActions(this, m_gantry.Equipment); //for now we just assume there's just one in reach
+		}
+	}
 }
